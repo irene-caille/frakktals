57e8854eb8c47248c5aac0dd25f13fae14578e8a
achtaitaipai <charles.cailleteau@gmail.com>
2023-11-27 19:34:53 +0100

    tailwind config height screen

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,12 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			height: {
+				screen: ['100vh', '100dvh']
+			},
+			minHeight: {
+				screen: ['100vh', '100dvh']
+			},
 			colors: {
 				fluo: '#FFFF00'
 			},

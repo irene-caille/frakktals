a88a888f6d7ca1d48a143ec64f6beef6e21595b6
achtaitaipai <charles.cailleteau@gmail.com>
2023-12-04 23:04:35 +0100

    fix slide mobile

--- src/libs/createPlaceHolder.ts
+++ src/libs/createPlaceHolder.ts
@@ -1,4 +1,4 @@
 import type { ImageMetadata } from "astro"
 import { getImage } from "astro:assets"
 
-export const createPlaceHolder = async (src:ImageMetadata) => await getImage({ src, width: 4,  format:'png', })
+export const createPlaceHolder = async (src:ImageMetadata) => await getImage({ src, width: 12,  format:'png', })

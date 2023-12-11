02eb60e98daf4e8f1a74cc2130d0d3ad41411c4b
achtaitaipai <charles.cailleteau@gmail.com>
2023-12-11 23:18:02 +0100

    images alias and remark replace images url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import {replaceImagesUrl} from './replaceImagesPaths.js'
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [replaceImagesUrl]
+  },
   integrations: [tailwind(), svelte()]
 });
